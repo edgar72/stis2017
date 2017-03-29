c2b77b786133250ba1d93f301c489848389c4f1a
pc <pc@172.16.17.141>
2017-03-29 13:36:02 -0600

    iniciacion del primero proyecto en git

---
+++ src/stis2017/Stis2017.java
@@ -0,0 +1,17 @@
+
+package stis2017;
+
+/**
+ *
+ * @author pc
+ */
+public class Stis2017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
